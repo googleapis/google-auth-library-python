ca39473e64ace7e2f9ae87d65b116f9dafcfb267
Jon Wayne Parrott <jonwayne@google.com>
2016-10-18 11:25:37 -0700

    Implement application default credentials

---
+++ google/auth/_default.py
@@ -0,0 +1,295 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Application default credentials.
+
+Implementes application default credentials and project ID detection."""
+
+import io
+import json
+import os
+
+from six.moves import configparser
+
+from google.auth import compute_engine
+from google.auth import exceptions
+from google.auth.compute_engine import _metadata
+import google.auth.transport._http_client
+from google.oauth2 import service_account
+import google.oauth2.credentials
+
+# Environment variable for explicit application default credentials and project
+# ID.
+_CREDENTIALS_ENV = 'GOOGLE_APPLICATION_CREDENTIALS'
+_PROJECT_ENV = 'GCLOUD_PROJECT'
+
+# Valid types accepted for file-based credentials.
+_AUTHORIZED_USER_TYPE = 'authorized_user'
+_SERVICE_ACCOUNT_TYPE = 'service_account'
+_VALID_TYPES = (_AUTHORIZED_USER_TYPE, _SERVICE_ACCOUNT_TYPE)
+
+# The Google OAuth 2.0 token endpoint. Used for authorized user credentials.
+_GOOGLE_OAUTH2_TOKEN_ENDPOINT = 'https://accounts.google.com/o/oauth2/token'
+
+# The ~/.config subdirectory containing gcloud credentials.
+_CLOUDSDK_CONFIG_DIRECTORY = 'gcloud'
+# The environment variable name which can replace ~/.config if set.
+_CLOUDSDK_CONFIG_ENV = 'CLOUDSDK_CONFIG'
+# The name of the file in the Cloud SDK config that contains default
+# credentials.
+_CLOUDSDK_CREDENTIALS_FILENAME = 'application_default_credentials.json'
+# The name of the file in the Cloud SDK config that contains the
+# active configuration.
+_CLOUDSDK_ACTIVE_CONFIG_FILENAME = os.path.join(
+    'configurations', 'config_default')
+# The config section and key for the project ID in the cloud SDK config.
+_CLOUDSDK_PROJECT_CONFIG_SECTION = 'core'
+_CLOUDSDK_PROJECT_CONFIG_KEY = 'project'
+
+# Help message when no credentials can be found.
+_HELP_MESSAGE = (
+    'Could not automatically determine credentials. Please set {env} or '
+    'explicitly create credential and re-run the application. For more '
+    'information, please see https://developers.google.com/accounts/docs'
+    '/application-default-credentials.'.format(env=_CREDENTIALS_ENV))
+
+
+def _load_credentials_from_file(filename):
+    """Loads credentials from a file.
+
+    The credentials file must be a service account key or stored authorized
+    user credentials.
+
+    Args:
+        filename (str): The full path to the credentials file.
+
+    Returns:
+        Tuple[google.auth.credentials.Credentials, Optional[str]]: Loaded
+            credentials and the project ID. Authorized user credentials do not
+            have the project ID information.
+
+    Raises:
+        google.auth.exceptions.DefaultCredentialsError: if the file is in the
+            wrong format.
+    """
+    with io.open(filename, 'r') as file_obj:
+        try:
+            info = json.load(file_obj)
+        except ValueError as exc:
+            raise exceptions.DefaultCredentialsError(
+                'File {} is not a valid json file.'.format(filename), exc)
+
+    # The type key should indicate that the file is either a service account
+    # credentials file or an authorized user credentials file.
+    credential_type = info.get('type')
+
+    if credential_type == _AUTHORIZED_USER_TYPE:
+        credentials = google.oauth2.credentials.Credentials(
+            None,
+            refresh_token=info['refresh_token'],
+            token_uri=_GOOGLE_OAUTH2_TOKEN_ENDPOINT,
+            client_id=info['client_id'],
+            client_secret=info['client_secret'])
+        # Authorized user credentials do not contain the project ID.
+        return credentials, None
+
+    if credential_type == _SERVICE_ACCOUNT_TYPE:
+        credentials = service_account.Credentials.from_service_account_info(
+            info)
+        return credentials, info.get('project_id')
+
+    else:
+        raise exceptions.DefaultCredentialsError(
+            'The file {file} does not have a valid type. '
+            'Type is {type}, expected one of {valid_types}.'.format(
+                file=filename, type=credential_type, valid_types=_VALID_TYPES))
+
+
+def _get_explicit_environ_credentials():
+    """Gets credentials from the GOOGLE_APPLICATION_CREDENTIALS environment
+    variable."""
+    explicit_file = os.environ.get(_CREDENTIALS_ENV)
+    if explicit_file is not None:
+        return _load_credentials_from_file(os.environ[_CREDENTIALS_ENV])
+    else:
+        return None, None
+
+
+def _get_gcloud_sdk_project_id(config_path):
+    """Gets the project ID from the Cloud SDK's configuration.
+
+    Args:
+        config_path (str): The path to the Cloud SDK's config directory,
+            for example ``~/.config/gcloud``.
+
+    Returns:
+        Optional[str]: The project ID.
+    """
+    config_file = os.path.join(config_path, _CLOUDSDK_ACTIVE_CONFIG_FILENAME)
+
+    if not os.path.exists(config_file):
+        return None
+
+    config = configparser.RawConfigParser()
+
+    try:
+        config.read(config_file)
+    except configparser.Error:
+        return None
+
+    if config.has_section(_CLOUDSDK_PROJECT_CONFIG_SECTION):
+        return config.get(
+            _CLOUDSDK_PROJECT_CONFIG_SECTION, _CLOUDSDK_PROJECT_CONFIG_KEY)
+
+
+def _get_gcloud_sdk_config_path():
+    """Returns the absolute path the the Cloud SDK's configuration directory.
+
+    Returns:
+        str: The Cloud SDK config path.
+    """
+    if _CLOUDSDK_CONFIG_ENV in os.environ:
+        return os.environ[_CLOUDSDK_CONFIG_ENV]
+
+    # Non-windows systems store this at ~/.config/gcloud
+    if os.name != 'nt':
+        return os.path.join(
+            os.path.expanduser('~'), '.config', _CLOUDSDK_CONFIG_DIRECTORY)
+    # Windows systems store config at %APPDATA%\gcloud
+    else:
+        if 'APPDATA' in os.environ:
+            return os.path.join(
+                os.environ['APPDATA'], _CLOUDSDK_CONFIG_DIRECTORY)
+        else:
+            # This should never happen unless someone is really
+            # messing with things, but we'll cover the case anyway.
+            drive = os.environ.get('SystemDrive', 'C:')
+            return os.path.join(
+                drive, '\\', _CLOUDSDK_CONFIG_DIRECTORY)
+
+
+def _get_gcloud_sdk_credentials():
+    """Gets the credentials and project ID from the Cloud SDK."""
+    # Get the Cloud SDK's configuration path.
+    config_path = _get_gcloud_sdk_config_path()
+
+    # Check the config path for the credentials file.
+    credentials_filename = os.path.join(
+        config_path, _CLOUDSDK_CREDENTIALS_FILENAME)
+
+    if not os.path.exists(credentials_filename):
+        return None, None
+
+    credentials, project_id = _load_credentials_from_file(
+        credentials_filename)
+
+    if not project_id:
+        project_id = _get_gcloud_sdk_project_id(config_path)
+
+    return credentials, project_id
+
+
+def _get_gae_credentials():
+    """Gets Google App Engine App Identity credentials and project ID."""
+    return None, None
+
+
+def _get_gce_credentials():
+    """Gets credentials and project ID from the GCE Metadata Service."""
+    # Ping requires a transport, but we want application default credentials
+    # to require no arguments. So, we'll use the _http_client transport which
+    # uses http.client. This is only acceptable because the metadata server
+    # doesn't do SSL and never requires proxies.
+    request = google.auth.transport._http_client.Request()
+
+    if _metadata.ping(request=request):
+        # Get the project ID.
+        try:
+            project_id = _metadata.get(request, 'project/project-id')
+        except exceptions.TransportError:
+            project_id = None
+
+        return compute_engine.Credentials(), project_id
+    else:
+        return None, None
+
+
+def default():
+    """Gets the default credentials for the current environment.
+
+    `Application Default Credentials`_ provides an easy way to obtain
+    credentials to call Google APIs for server-to-server or local applications.
+    This function acquires credentials from the environment in the following
+    order:
+
+    1. If the environment variable ``GOOGLE_APPLICATION_CREDENTIALS`` is set
+       to the path of a valid service account private key file, then it is
+       loaded and returned. The project ID returned is the project ID defined
+       in the service account file.
+    2. If the `Google Cloud SDK`_ is installed and has credentials set via
+       ``gcloud auth application-default login`` they are loaded and returned.
+       The Project ID is the project ID configured with ``gcloud init`` or
+       ``gcloud config set project``.
+    3. If the application is running in the `App Engine standard environment`_
+       then the credentials and project ID from the `App Identity Service`_
+       are used.
+    4. If the application is running in `Compute Engine`_ or the
+       `App Engine flexible environment`_ then the credentials and project ID
+       are obtained from the `Metadata Service`_.
+    5. If no credentials are found,
+       :class:`~google.auth.exceptions.DefaultCredentialsError` will be raised.
+
+    .. _Application Default Credentials: https://developers.google.com\
+            /identity/protocols/application-default-credentials
+    .. _Google Cloud SDK: https://cloud.google.com/sdk
+    .. _App Engine standard environment: https://cloud.google.com/appengine
+    .. _App Identity Service: https://cloud.google.com/appengine/docs/python\
+            /appidentity/
+    .. _Compute Engine: https://cloud.google.com/compute
+    .. _App Engine flexible environment: https://cloud.google.com\
+            /appengine/flexible
+    .. _Metadata Service: https://cloud.google.com/compute/docs\
+            /storing-retrieving-metadata
+
+    Example::
+
+        import google.auth
+
+        credentials, project_id = google.auth.default()
+
+    Returns:
+        Tuple[~google.auth.credentials.Credentials, Optional[str]]:
+            the current environment's credentials and project ID. Project ID
+            may be None, which indicates that the Project ID could not be
+            ascertained from the environment.
+
+    Raises:
+        ~google.auth.exceptions.DefaultCredentialsError:
+            If no credentials were found, or if the credentials found were
+            invalid.
+    """
+    explicit_project_id = os.environ.get(_PROJECT_ENV)
+
+    checkers = (
+        _get_explicit_environ_credentials,
+        _get_gcloud_sdk_credentials,
+        _get_gae_credentials,
+        _get_gce_credentials)
+
+    for checker in checkers:
+        credentials, project_id = checker()
+        if credentials is not None:
+            return credentials, explicit_project_id or project_id
+
+    raise exceptions.DefaultCredentialsError(_HELP_MESSAGE)
---
+++ tests/test__default.py
@@ -0,0 +1,284 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+
+import mock
+import pytest
+
+from google.auth import _default
+from google.auth import compute_engine
+from google.auth import exceptions
+from google.oauth2 import service_account
+import google.oauth2.credentials
+
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+AUTHORIZED_USER_FILE = os.path.join(DATA_DIR, 'authorized_user.json')
+
+with open(AUTHORIZED_USER_FILE) as fh:
+    AUTHORIZED_USER_FILE_DATA = json.load(fh)
+
+SERVICE_ACCOUNT_FILE = os.path.join(DATA_DIR, 'service_account.json')
+
+with open(SERVICE_ACCOUNT_FILE) as fh:
+    SERVICE_ACCOUNT_FILE_DATA = json.load(fh)
+
+with open(os.path.join(DATA_DIR, 'cloud_sdk.cfg')) as fh:
+    CLOUD_SDK_CONFIG_DATA = fh.read()
+
+
+def test__load_credentials_from_file_invalid_json(tmpdir):
+    jsonfile = tmpdir.join('invalid.json')
+    jsonfile.write('{')
+
+    with pytest.raises(exceptions.DefaultCredentialsError) as excinfo:
+        _default._load_credentials_from_file(str(jsonfile))
+
+    assert excinfo.match(r'not a valid json file')
+
+
+def test__load_credentials_from_file_invalid_type(tmpdir):
+    jsonfile = tmpdir.join('invalid.json')
+    jsonfile.write(json.dumps({'type': 'not-a-real-type'}))
+
+    with pytest.raises(exceptions.DefaultCredentialsError) as excinfo:
+        _default._load_credentials_from_file(str(jsonfile))
+
+    assert excinfo.match(r'does not have a valid type')
+
+
+def test__load_credentials_from_file_authorized_user():
+    credentials, project_id = _default._load_credentials_from_file(
+        AUTHORIZED_USER_FILE)
+    assert isinstance(credentials, google.oauth2.credentials.Credentials)
+    assert project_id is None
+    assert credentials.token is None
+    assert (credentials._refresh_token ==
+            AUTHORIZED_USER_FILE_DATA['refresh_token'])
+    assert credentials._client_id == AUTHORIZED_USER_FILE_DATA['client_id']
+    assert (credentials._client_secret ==
+            AUTHORIZED_USER_FILE_DATA['client_secret'])
+    assert credentials._token_uri == _default._GOOGLE_OAUTH2_TOKEN_ENDPOINT
+
+
+def test__load_credentials_from_file_service_account():
+    credentials, project_id = _default._load_credentials_from_file(
+        SERVICE_ACCOUNT_FILE)
+    assert isinstance(credentials, service_account.Credentials)
+    assert project_id == SERVICE_ACCOUNT_FILE_DATA['project_id']
+
+
+@mock.patch.dict(os.environ, {}, clear=True)
+def test__get_explicit_environ_credentials_no_env():
+    assert _default._get_explicit_environ_credentials() == (None, None)
+
+
+LOAD_FILE_PATCH = mock.patch(
+    'google.auth._default._load_credentials_from_file', return_value=(
+        mock.sentinel.credentials, mock.sentinel.project_id))
+
+
+@LOAD_FILE_PATCH
+def test__get_explicit_environ_credentials(mock_load, monkeypatch):
+    monkeypatch.setenv(_default._CREDENTIALS_ENV, 'filename')
+
+    credentials, project_id = _default._get_explicit_environ_credentials()
+
+    assert credentials is mock.sentinel.credentials
+    assert project_id is mock.sentinel.project_id
+    mock_load.assert_called_with('filename')
+
+
+def test__get_gcloud_sdk_project_id(tmpdir):
+    config_dir = tmpdir.join(
+        '.config', _default._CLOUDSDK_CONFIG_DIRECTORY)
+    config_file = config_dir.join(
+        _default._CLOUDSDK_ACTIVE_CONFIG_FILENAME)
+    config_file.write(CLOUD_SDK_CONFIG_DATA, ensure=True)
+
+    project_id = _default._get_gcloud_sdk_project_id(str(config_dir))
+
+    assert project_id == 'example-project'
+
+
+def test__get_gcloud_sdk_project_id_non_existent(tmpdir):
+    project_id = _default._get_gcloud_sdk_project_id(str(tmpdir))
+    assert project_id is None
+
+
+def test__get_gcloud_sdk_project_id_bad_file(tmpdir):
+    config_dir = tmpdir.join(
+        '.config', _default._CLOUDSDK_CONFIG_DIRECTORY)
+    config_file = config_dir.join(
+        _default._CLOUDSDK_ACTIVE_CONFIG_FILENAME)
+    config_file.write('<<<badconfig', ensure=True)
+
+    project_id = _default._get_gcloud_sdk_project_id(str(config_dir))
+
+    assert project_id is None
+
+
+def test__get_gcloud_sdk_project_id_no_section(tmpdir):
+    config_dir = tmpdir.join(
+        '.config', _default._CLOUDSDK_CONFIG_DIRECTORY)
+    config_file = config_dir.join(
+        _default._CLOUDSDK_ACTIVE_CONFIG_FILENAME)
+    config_file.write('[section]', ensure=True)
+
+    project_id = _default._get_gcloud_sdk_project_id(str(config_dir))
+
+    assert project_id is None
+
+
+@LOAD_FILE_PATCH
+def test__get_gcloud_sdk_credentials_explicit_path(
+        mock_load, monkeypatch, tmpdir):
+    filename = tmpdir.join(_default._CLOUDSDK_CREDENTIALS_FILENAME)
+    filename.ensure()
+    monkeypatch.setenv(_default._CLOUDSDK_CONFIG_ENV, str(tmpdir))
+
+    credentials, project_id = _default._get_gcloud_sdk_credentials()
+
+    assert credentials is mock.sentinel.credentials
+    assert project_id is mock.sentinel.project_id
+    mock_load.assert_called_with(str(filename))
+
+
+def test__get_gcloud_sdk_credentials_non_existent(monkeypatch, tmpdir):
+    tmpdir.join(_default._CLOUDSDK_CREDENTIALS_FILENAME)
+    monkeypatch.setenv(_default._CLOUDSDK_CONFIG_ENV, str(tmpdir))
+
+    credentials, project_id = _default._get_gcloud_sdk_credentials()
+
+    assert credentials is None
+    assert project_id is None
+
+
+@mock.patch('os.path.expanduser')
+def test__get_gcloud_sdk_config_path_unix(
+        mock_expanduser):
+    mock_expanduser.side_effect = lambda path: path
+
+    config_path = _default._get_gcloud_sdk_config_path()
+
+    assert os.path.split(config_path) == (
+        '~/.config', _default._CLOUDSDK_CONFIG_DIRECTORY)
+
+
+@mock.patch('os.name', new='nt')
+def test__get_gcloud_sdk_config_path_windows(monkeypatch):
+    monkeypatch.setenv('APPDATA', 'appdata')
+
+    config_path = _default._get_gcloud_sdk_config_path()
+
+    assert os.path.split(config_path) == (
+        'appdata', _default._CLOUDSDK_CONFIG_DIRECTORY)
+
+
+@mock.patch('os.name', new='nt')
+def test__get_gcloud_sdk_config_path_no_appdata(monkeypatch):
+    monkeypatch.delenv('APPDATA', raising=False)
+    monkeypatch.setenv('SystemDrive', 'G:')
+
+    config_path = _default._get_gcloud_sdk_config_path()
+
+    assert os.path.split(config_path) == (
+        'G:/\\', _default._CLOUDSDK_CONFIG_DIRECTORY)
+
+
+@mock.patch(
+    'google.auth._default._get_gcloud_sdk_project_id',
+    return_value=mock.sentinel.project_id)
+@mock.patch('os.path.exists', return_value=True)
+@LOAD_FILE_PATCH
+def test__get_gcloud_sdk_credentials_no_project_id(
+        mock_load, unused_mock_exists, mock_get_project_id):
+    mock_load.return_value = (mock.sentinel.credentials, None)
+
+    credentials, project_id = _default._get_gcloud_sdk_credentials()
+
+    assert credentials == mock.sentinel.credentials
+    assert project_id == mock.sentinel.project_id
+    assert mock_get_project_id.called
+
+
+def test__get_gae_credentials():
+    assert _default._get_gae_credentials() == (None, None)
+
+
+@mock.patch(
+    'google.auth.compute_engine._metadata.ping', return_value=True)
+@mock.patch(
+    'google.auth.compute_engine._metadata.get', return_value='example-project')
+def test__get_gce_credentials(get_mock, ping_mock):
+    credentials, project_id = _default._get_gce_credentials()
+
+    assert isinstance(credentials, compute_engine.Credentials)
+    assert project_id == 'example-project'
+
+
+@mock.patch('google.auth.compute_engine._metadata.ping', return_value=False)
+def test__get_gce_credentials_no_ping(ping_mock):
+    credentials, project_id = _default._get_gce_credentials()
+
+    assert credentials is None
+    assert project_id is None
+
+
+@mock.patch(
+    'google.auth.compute_engine._metadata.ping', return_value=True)
+@mock.patch(
+    'google.auth.compute_engine._metadata.get',
+    side_effect=exceptions.TransportError())
+def test__get_gce_credentials_no_project_id(get_mock, ping_mock):
+    credentials, project_id = _default._get_gce_credentials()
+
+    assert isinstance(credentials, compute_engine.Credentials)
+    assert project_id is None
+
+
+@mock.patch(
+    'google.auth._default._get_explicit_environ_credentials',
+    return_value=(mock.sentinel.credentials, mock.sentinel.project_id))
+def test_default_early_out(get_mock):
+    assert _default.default() == (
+        mock.sentinel.credentials, mock.sentinel.project_id)
+
+
+@mock.patch(
+    'google.auth._default._get_explicit_environ_credentials',
+    return_value=(mock.sentinel.credentials, mock.sentinel.project_id))
+def test_default_explict_project_id(get_mock, monkeypatch):
+    monkeypatch.setenv(_default._PROJECT_ENV, 'explicit-env')
+    assert _default.default() == (
+        mock.sentinel.credentials, 'explicit-env')
+
+
+@mock.patch(
+    'google.auth._default._get_explicit_environ_credentials',
+    return_value=(None, None))
+@mock.patch(
+    'google.auth._default._get_gcloud_sdk_credentials',
+    return_value=(None, None))
+@mock.patch(
+    'google.auth._default._get_gae_credentials',
+    return_value=(None, None))
+@mock.patch(
+    'google.auth._default._get_gce_credentials',
+    return_value=(None, None))
+def test_default_fail(unused_gce, unused_gae, unused_sdk, unused_explicit):
+    with pytest.raises(exceptions.DefaultCredentialsError):
+        assert _default.default()
