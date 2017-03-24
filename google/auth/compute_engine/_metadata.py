24850d22230b99bdffafc3f2229826f5b89da9ea
Craig Citro <craigcitro@google.com>
2017-03-24 14:32:09 -0700

    Allow customizing the GCE metadata service address via an env var.
    The goal here is to make it possible for a user of a binary that depends on
    this library (eg the google cloud SDK) to be able to customize where it looks
    for the GCE metadata service. (An adventurous user can already customize the
    GCE metadata service location via the existing global vars in this library.)
    
    The only bit of awkwardness here is really the test: since this is a top-level
    statement, reloading is the only way to ensure it works.

--- google/auth/compute_engine/_metadata.py
+++ google/auth/compute_engine/_metadata.py
@@ -30,11 +30,12 @@ from google.auth import exceptions
 
 _LOGGER = logging.getLogger(__name__)
 
-_METADATA_ROOT = 'http://metadata.google.internal/computeMetadata/v1/'
+_METADATA_ROOT = 'http://{}/computeMetadata/v1/'.format(
+    os.getenv('GCE_METADATA_ROOT', 'metadata.google.internal'))
 
 # This is used to ping the metadata server, it avoids the cost of a DNS
 # lookup.
-_METADATA_IP_ROOT = 'http://169.254.169.254'
+_METADATA_IP_ROOT = 'http://' + os.getenv('GCE_METADATA_IP', '169.254.169.254')
 _METADATA_FLAVOR_HEADER = 'metadata-flavor'
 _METADATA_FLAVOR_VALUE = 'Google'
 _METADATA_HEADERS = {_METADATA_FLAVOR_HEADER: _METADATA_FLAVOR_VALUE}
