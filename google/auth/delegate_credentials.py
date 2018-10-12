c872ca6ef2f771ae01a6f633391a44797cf02d0a
salmaan rashid <salrashid123@gmail.com>
2018-10-12 15:15:49 -0700

    Add delegate_credentials

---
+++ google/auth/delegate_credentials.py
@@ -0,0 +1,157 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Delegated credentials.
+
+This module provides authentication for applications where local credentials 
+impersonates a remote service account using `IAM Credentials API`_.
+
+This class can be used to impersonate a service account as long as the original
+Credential object as the "Service Account Token Creator" role on the target 
+service account.
+
+.. _IAM Credentials API:
+    https://cloud.google.com/iam/credentials/reference/rest/
+
+"""
+
+import json
+import copy
+import datetime
+
+from google.auth import _helpers
+from google.auth import credentials
+
+from google.auth.transport.requests import Request, AuthorizedSession
+from google.auth import exceptions 
+
+_DEFAULT_TOKEN_LIFETIME_SECS = 3600 # 1 hour in seconds
+
+_IAM_SCOPE = ['https://www.googleapis.com/auth/iam']
+
+# Number of seconds before token expiration to initiate a refresh
+_CLOCK_SKEW_SECS = 30
+_CLOCK_SKEW = datetime.timedelta(seconds=_CLOCK_SKEW_SECS)
+
+class DelegateCredentials(credentials.Credentials):
+    """Delegate credentials
+
+    These are delegate credentials which are essentially impersonated identities.
+
+    When the `Service Account Token Creator` IAM role is granted to a service account,
+    any other identity that has that capability can impersonate that service account.
+    For more information about Token Creator IAM role and IAMCredentials API, see
+     `IAM documentation`_.
+
+    .. _The Service Account Token Creator role:
+        https://cloud.google.com/iam/docs/service-accounts#the_service_account_token_creator_role
+
+    Usage:
+    First grant root_credentials the `Service Account Token Creator` role on
+    the account to impersonate.   In this example, the service account represented by
+    svc_account.json has the token creator role on 
+    `impersonated-account@_project_.iam.gserviceaccount.com`.
+
+    Second, enable 'iamcredentials.googleapis.com' API on the project represented by 
+    `svc_account.json`
+
+        # initialize root_credentials for any scope/usage
+        scopes = ['https://www.googleapis.com/auth/devstorage.read_only']
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/path/to/svc_account.json'
+        root_credentials, project = google.auth.default(scopes=scopes)
+        
+        # now use the root_credentials to initalize a delegate_credentials
+        new_scopes = ['https://www.googleapis.com/auth/devstorage.read_only']
+        delegate_credentials = DelegateCredentials(
+              root_credentials = apex_credentials,
+              principal='impersonated-account@_project_.iam.gserviceaccount.com',
+              new_scopes = new_scopes,
+              delegates=[],
+              lifetime=500)
+
+        # now list the bucket as the delegate.  The root credential does
+        # not need to have access to this project but the delegate does
+        client = storage.Client(credentials=delegate_credentials)
+        buckets = client.list_buckets(project='your_project')
+        for bkt in buckets:
+          print bkt.name
+    """
+
+    def __init__(self, root_credentials,  principal,
+              new_scopes, delegates=None,
+              lifetime=_DEFAULT_TOKEN_LIFETIME_SECS):
+        """
+        Args:
+            root_credentials (google.auth.Credentials): The root credential
+                used as to acquire the delegated credentials.
+            principal (str): The service account to impersonatge.
+            new_scopes (Sequence[str]): Scopes to request during the authorization
+                grant.
+            delegates (Sequence[str]): The chained list of delegates required to grant
+                the final access_token
+            lifetime (int): Number of seconds the delegated credential should be valid for
+                (max 3600).
+        """              
+
+        super(credentials.Credentials, self).__init__()
+
+        self._root_credentials = copy.copy(root_credentials)
+        self._root_credentials._scopes = _IAM_SCOPE
+        self._principal = principal
+        self._new_scopes = new_scopes
+        self._delegates = delegates
+        self._lifetime = lifetime
+        self.token = None
+        self.expiry = _helpers.utcnow()
+
+    @_helpers.copy_docstring(credentials.Credentials)
+    def refresh(self, request):
+        self._root_credentials.refresh(request)
+        self._updateToken()
+
+    @property
+    def expired(self):
+        skewed_expiry = self.expiry - _CLOCK_SKEW
+        return _helpers.utcnow() >= skewed_expiry
+
+    def _updateToken(self):
+        """Updates the delegate credentials with a new access_token representing 
+        the delegated account.
+
+        Raises:
+            DefaultCredentialsError: Raised if the delegated credentials are not 
+               available.  Common reasons are `iamcredentials.googleapis.com` is
+               not enabled or the `Service Account Token Creator` is not assigned
+        """
+        req = Request()
+        self._root_credentials.refresh(req)
+
+        body={
+            "delegates": self._delegates,
+            "scope": self._new_scopes,
+            "lifetime": str(self._lifetime) + "s"
+        }
+        iam_endpoint = ('https://iamcredentials.googleapis.com/v1/projects/'
+                        '-/serviceAccounts/{}:generateAccessToken'.format(self._principal))
+        try:
+            authed_session = AuthorizedSession(self._root_credentials)
+            response = authed_session.post(iam_endpoint, headers={'Content-Type': 'application/json'}, json=body)
+            if (response.status_code == 200):
+                token_response = json.loads(response.content)
+                self.token = token_response['accessToken']
+                self.expiry = datetime.datetime.strptime(token_response['expireTime'], '%Y-%m-%dT%H:%M:%SZ')
+            else:
+              raise exceptions.DefaultCredentialsError("Unable to acquire delegated credentials " + self._principal)
+        except (exceptions.TransportError, ValueError, KeyError) as e:
+            raise exceptions.DefaultCredentialsError("Unable to acquire delegated credentials " + self._principal)
---
+++ tests/test_delegate_credentials.py
@@ -0,0 +1,148 @@
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+import json
+import os
+import mock
+import pytest
+
+from six.moves import http_client
+
+from google.auth import exceptions
+from google.auth.delegate_credentials import DelegateCredentials
+
+from google.auth import _helpers
+from google.auth import crypt
+from google.auth import transport
+from google.oauth2 import service_account
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), '', 'data')
+
+with open(os.path.join(DATA_DIR, 'privatekey.pem'), 'rb') as fh:
+    PRIVATE_KEY_BYTES = fh.read()
+
+SERVICE_ACCOUNT_JSON_FILE = os.path.join(DATA_DIR, 'service_account.json')
+
+with open(SERVICE_ACCOUNT_JSON_FILE, 'r') as fh:
+    SERVICE_ACCOUNT_INFO = json.load(fh)
+
+SIGNER = crypt.RSASigner.from_string(PRIVATE_KEY_BYTES, '1')
+
+class TestDelegateCredentials(object):
+    SERVICE_ACCOUNT_EMAIL = 'service-account@example.com'
+    IMPERSONATED_ACCOUNT = 'impersonated-account@project.iam.gserviceaccount.com'
+    NEW_SCOPES = ['https://www.googleapis.com/auth/devstorage.read_only']
+    DELEGATES = []
+    NEW_SCOPES = 3600
+
+    TOKEN_URI = 'https://example.com/oauth2/token'
+
+    @classmethod
+    def make_credentials(cls):
+        root_credentials = service_account.Credentials(
+            SIGNER, cls.SERVICE_ACCOUNT_EMAIL, cls.TOKEN_URI)
+        return DelegateCredentials(
+              root_credentials = root_credentials,
+              principal = cls.IMPERSONATED_ACCOUNT,
+              new_scopes = cls.NEW_SCOPES,
+              delegates = cls.DELEGATES,
+              lifetime = cls.NEW_SCOPES)
+
+    def test_default_state(self):
+        credentials = self.make_credentials()
+        assert not credentials.valid
+        assert credentials.expired   
+
+
+    @mock.patch('google.oauth2._client.jwt_grant', autospec=True)
+    def test_refresh_success(self, jwt_grant):
+        credentials = self.make_credentials()
+        token = 'token'
+
+        jwt_grant.return_value = (
+            token,
+            _helpers.utcnow() + datetime.timedelta(seconds=500),
+            {})
+
+        request_body={
+            "delegates": credentials._delegates,
+            "scope": credentials._new_scopes,
+            "lifetime": str(credentials._lifetime) + "s"
+        }
+        response_body={
+            "accessToken": token,
+            "expireTime": (_helpers.utcnow() + datetime.timedelta(seconds=credentials._lifetime)).isoformat('T') + 'Z'
+        }            
+                
+        response = mock.create_autospec(transport.Response, instance=True)
+        response.status = http_client.OK
+        response.data = _helpers.to_bytes( json.dumps(response_body) )
+
+        request = mock.create_autospec(transport.Request, instance=True)
+        request.data = _helpers.to_bytes( json.dumps(request_body) )
+        request.return_value = response
+
+        credentials.refresh(request)
+
+        assert credentials.valid
+        assert not credentials.expired
+
+    @mock.patch('google.oauth2._client.jwt_grant', autospec=True)
+    def test_refresh_failure(self, jwt_grant):
+        credentials = self.make_credentials()
+        token = 'token'
+        jwt_grant.return_value = (
+            token,
+            _helpers.utcnow() + datetime.timedelta(seconds=500),
+            {})
+
+        body={
+            "delegates": credentials._delegates,
+            "scope": credentials._new_scopes,
+            "lifetime": str(credentials._lifetime) + "s"
+        }
+        response = mock.create_autospec(transport.Response, instance=True)
+        response.status = http_client.NOT_FOUND
+        response.headers = {}
+        request = mock.create_autospec(transport.Request)
+        request.return_value = response
+        request.headers = {}
+        request.data = _helpers.to_bytes( json.dumps(body) )
+
+        #with pytest.raises(exceptions.TransportError) as excinfo:
+        #  credentials.refresh(request)
+
+        with pytest.raises(exceptions.DefaultCredentialsError) as excinfo:
+          credentials.refresh(request)
+
+        assert excinfo.match(r'Unable to acquire delegated credentials ')
+        assert not credentials.valid
+        assert credentials.expired     
+
+    def test_expired(self):
+        credentials = self.make_credentials()
+        assert credentials.expired
+
+
+
+    def test___updateToken(self):
+        credentials = self.make_credentials()
+
+        body={
+            "delegates": credentials._delegates,
+            "scope": credentials._new_scopes,
+            "lifetime": str(credentials._lifetime) + "s"
+        }
+        
