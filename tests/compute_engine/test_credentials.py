393d9ef13fbca416d9bed09c63e15dd4ae9042da
Brent Shaffer <betterbrent@google.com>
2020-02-11 09:24:20 -0800

    fixes lint

--- tests/compute_engine/test_credentials.py
+++ tests/compute_engine/test_credentials.py
@@ -294,8 +294,8 @@ class TestIDTokenCredentials(object):
             json={
                 "scopes": "email",
                 "email": "service-account@example.com",
-                "aliases": ["default"]
-            }
+                "aliases": ["default"],
+            },
         )
 
         # mock token for credentials
@@ -308,28 +308,26 @@ class TestIDTokenCredentials(object):
             json={
                 "access_token": "some-token",
                 "expires_in": 3210,
-                "token_type": "Bearer"
-            }
+                "token_type": "Bearer",
+            },
         )
 
         # mock sign blob endpoint
-        signature = base64.b64encode(b"some-signature").decode('utf-8')
+        signature = base64.b64encode(b"some-signature").decode("utf-8")
         responses.add(
             responses.POST,
             "https://iam.googleapis.com/v1/projects/-/serviceAccounts/"
             "service-account@example.com:signBlob?alt=json",
             status=200,
             content_type="application/json",
-            json={
-                "keyId": "some-key-id",
-                "signature": signature
-            }
+            json={"keyId": "some-key-id", "signature": signature},
         )
 
         id_token = "{}.{}.{}".format(
-            base64.b64encode(b'{"some":"some"}').decode('utf-8'),
-            base64.b64encode(b'{"exp": 3210}').decode('utf-8'),
-            base64.b64encode(b"token").decode('utf-8'))
+            base64.b64encode(b'{"some":"some"}').decode("utf-8"),
+            base64.b64encode(b'{"exp": 3210}').decode("utf-8"),
+            base64.b64encode(b"token").decode("utf-8"),
+        )
 
         # mock id token endpoint
         responses.add(
@@ -337,19 +335,16 @@ class TestIDTokenCredentials(object):
             "https://www.googleapis.com/oauth2/v4/token",
             status=200,
             content_type="application/json",
-            json={
-                "id_token": id_token,
-                "expiry": 3210
-            }
+            json={"id_token": id_token, "expiry": 3210},
         )
 
         self.credentials = credentials.IDTokenCredentials(
             request=requests.Request(),
             service_account_email="service-account@example.com",
-            target_audience="https://audience.com")
+            target_audience="https://audience.com",
+        )
 
-        self.credentials = (
-            self.credentials.with_target_audience("https://actually.not"))
+        self.credentials = self.credentials.with_target_audience("https://actually.not")
 
         self.credentials.refresh(requests.Request())
 
