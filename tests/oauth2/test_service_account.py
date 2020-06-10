bc8b40f24ce66fa780621c853cb84c42c4db21e6
Dov Shlachter <dovs@google.com>
2020-06-10 11:44:49 -0700

    Test coverage

--- tests/oauth2/test_service_account.py
+++ tests/oauth2/test_service_account.py
@@ -151,6 +151,9 @@ class TestCredentials(object):
         credentials = self.make_credentials()
         new_credentials = credentials.with_quota_project("new-project-456")
         assert new_credentials.quota_project_id == "new-project-456"
+        hdrs = {}
+        new_credentials.apply(hdrs, token="tok")
+        assert "x-goog-user-project" in hdrs
 
     def test__make_authorization_grant_assertion(self):
         credentials = self.make_credentials()
