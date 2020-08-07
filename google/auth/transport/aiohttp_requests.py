5e86c2342d828973ec6a6afa62b70c53ca66cbce
Chris Wilcox <crwilcox@google.com>
2020-08-06 22:40:25 -0700

    remove commented code

--- google/auth/transport/aiohttp_requests.py
+++ google/auth/transport/aiohttp_requests.py
@@ -33,6 +33,11 @@ _DEFAULT_TIMEOUT = 180  # in seconds
 
 
 class _CombinedResponse(transport.Response):
+    '''
+    In order to more closely resemble the `requests` interface, where a raw
+    and deflated content could be accessed at once, this class lazily reads the 
+    stream in `transport.Response` so both return forms can be used
+    '''
     def __init__(self, response):
         self._response = response
         self._raw_content = None
@@ -86,28 +91,6 @@ class _Response(transport.Response):
     @property
     def text(self):
         return self._response.text
-
-    # def _is_compressed(self):
-    #     # The gzip and deflate transfer-encodings are automatically decoded for you.
-    #     headers = self._client_response.headers
-    #     return "Content-Encoding" in headers and (
-    #         headers["Content-Encoding"] == "gzip"
-    #         or headers["Content-Encoding"] == "deflate"
-    #     )
-
-    # async def raw_content(self):
-    #     if self._raw_content is None:
-    #         self._raw_content = await self._response.content.read()
-    #     return self._raw_content
-
-    # async def content(self):
-    #     if self._raw_content is None:
-    #         self._raw_content = await self._response.content.read()
-    #     if self._is_compressed:
-    #         d = zlib.decompressobj(zlib.MAX_WBITS | 32)
-    #         decompressed = d.decompress(self._raw_content)
-    #         return decompressed
-    #     return self._raw_content
 
 
 class Request(transport.Request):
