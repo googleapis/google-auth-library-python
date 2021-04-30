d761a6d094f780a0f0ca971a4403b3a14ddf5da5
Bu Sun Kim <busunkim@google.com>
2021-04-30 22:25:12 +0000

    chore: add html redirects

--- make_redirects.py
+++ make_redirects.py
@@ -1,81 +1,24 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 
-static_html_pages = [
-    "search.html",
-    "genindex.html",
-    "py-modindex.html",
-    "user-guide.html",
-    "index.html",
-    "oauth2client-deprecation.html",
-    "_modules/abc.html",
-    "_modules/index.html",
-    "_modules/urllib3/request.html",
-    "_modules/aiohttp/client.html",
-    "_modules/requests/sessions.html",
-    "_modules/google/oauth2/utils.html",
-    "_modules/google/oauth2/_service_account_async.html",
-    "_modules/google/oauth2/_credentials_async.html",
-    "_modules/google/oauth2/sts.html",
-    "_modules/google/oauth2/credentials.html",
-    "_modules/google/oauth2/id_token.html",
-    "_modules/google/oauth2/service_account.html",
-    "_modules/google/auth/_credentials_async.html",
-    "_modules/google/auth/identity_pool.html",
-    "_modules/google/auth/transport.html",
-    "_modules/google/auth/app_engine.html",
-    "_modules/google/auth/_jwt_async.html",
-    "_modules/google/auth/_default.html",
-    "_modules/google/auth/iam.html",
-    "_modules/google/auth/jwt.html",
-    "_modules/google/auth/impersonated_credentials.html",
-    "_modules/google/auth/exceptions.html",
-    "_modules/google/auth/aws.html",
-    "_modules/google/auth/credentials.html",
-    "_modules/google/auth/external_account.html",
-    "_modules/google/auth/compute_engine/credentials.html",
-    "_modules/google/auth/transport/urllib3.html",
-    "_modules/google/auth/transport/grpc.html",
-    "_modules/google/auth/transport/_aiohttp_requests.html",
-    "_modules/google/auth/transport/mtls.html",
-    "_modules/google/auth/transport/requests.html",
-    "_modules/google/auth/crypt/_python_rsa.html",
-    "_modules/google/auth/crypt/base.html",
-    "reference/google.auth.credentials.html",
-    "reference/google.auth.jwt.html",
-    "reference/google.auth.crypt.html",
-    "reference/google.oauth2._credentials_async.html",
-    "reference/google.auth.exceptions.html",
-    "reference/google.auth.iam.html",
-    "reference/google.oauth2.sts.html",
-    "reference/google.oauth2.utils.html",
-    "reference/google.auth.aws.html",
-    "reference/google.auth.compute_engine.credentials.html",
-    "reference/google.auth.transport._aiohttp_requests.html",
-    "reference/google.auth.impersonated_credentials.html",
-    "reference/modules.html",
-    "reference/google.html",
-    "reference/google.auth.crypt.base.html",
-    "reference/google.oauth2.service_account.html",
-    "reference/google.oauth2.credentials.html",
-    "reference/google.oauth2._service_account_async.html",
-    "reference/google.auth.app_engine.html",
-    "reference/google.auth.transport.requests.html",
-    "reference/google.auth._jwt_async.html",
-    "reference/google.auth.external_account.html",
-    "reference/google.auth.transport.urllib3.html",
-    "reference/google.auth.transport.mtls.html",
-    "reference/google.auth.html",
-    "reference/google.auth._credentials_async.html",
-    "reference/google.auth.identity_pool.html",
-    "reference/google.oauth2.id_token.html",
-    "reference/google.auth.crypt.rsa.html",
-    "reference/google.auth.crypt.es256.html",
-    "reference/google.auth.compute_engine.html",
-    "reference/google.auth.environment_vars.html",
-    "reference/google.oauth2.html",
-    "reference/google.auth.transport.html",
-    "reference/google.auth.transport.grpc.html",
-]
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+from pathlib import Path
+
 
 def get_files_with_extension(dirName, extension=".html"):
     files = []
@@ -85,6 +28,7 @@ def get_files_with_extension(dirName, extension=".html"):
             files.append(str(file))
 
     return files
+
 
 def add_redirect(path, url):
     HTML_TEMPLATE = f"""<html>
@@ -131,11 +75,19 @@ def make_redirects(dirname, ext=".html"):
 
     return redirects
 
+
 if __name__ == "__main__":
     redirects = make_redirects(".")
 
+    retry_strategy = Retry(total=3,)
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    http = requests.Session()
+    http.mount("https://", adapter)
+    http.mount("http://", adapter)
+
     for path, url in redirects.items():
-        resp = requests.get(url)
+        print(url)
+        resp = http.get(url)
         if resp.status_code == 200:
             add_redirect(path, url)
         else:
