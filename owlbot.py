b685dbbe1815ea221a89269644fc95d6f9d44c87
Tres Seaver <tseaver@palladion.com>
2021-08-13 18:16:11 -0400

    chore: agonal respiration

--- owlbot.py
+++ owlbot.py
@@ -29,14 +29,14 @@ assert 1 == s.replace(
 assert 1 == s.replace(
     ".kokoro/docker/docs/Dockerfile",
     """\
-CMD ["python3.8"]""",
+CMD \["python3\.8"\]""",
     """\
 # Install gcloud SDK
-RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | \
-    tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
-  && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-    apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - \
-  && apt-get update -y \
+RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | \\
+    tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \\
+  && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \\
+    apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - \\
+  && apt-get update -y \\
   && apt-get install python2 google-cloud-sdk -y
 
 CMD ["python3.8"]""",
