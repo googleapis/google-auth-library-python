9d64d4822574441f04df52c4609faa139c9336a7
Bu Sun Kim <busunkim@google.com>
2021-09-09 20:38:11 +0000

    chore: artifically lower version to < 2 to appease pip

--- google/auth/compute_engine/_metadata.py
+++ google/auth/compute_engine/_metadata.py
@@ -262,5 +262,5 @@ def get_service_account_token(request, service_account="default", scopes=None):
     token_expiry = _helpers.utcnow() + datetime.timedelta(
         seconds=token_json["expires_in"]
     )
-    _LOGGER.debug(f"Token expires in {token_json["expires_in"]} seconds.")
+    _LOGGER.debug(f"Token expires in {token_json['expires_in']} seconds.")
     return token_json["access_token"], token_expiry
