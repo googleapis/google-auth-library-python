311ec5edf30b68b54b63dce2e95929ac89ac212b
Sijun Liu <liusijun1985@gmail.com>
2021-10-19 02:23:26 +0000

    fix: fix setup and tests

--- setup.py
+++ setup.py
@@ -61,10 +61,12 @@ setup(
     namespace_packages=("google",),
     install_requires=DEPENDENCIES,
     extras_require=extras,
-    python_requires=">= 3.6",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     license="Apache 2.0",
     keywords="google auth oauth client",
     classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
