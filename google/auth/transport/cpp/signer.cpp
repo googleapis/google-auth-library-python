a13dcbbe6730ee3976cf145cb7dae7e805beab49
user <user@DESKTOP-F38E1TO>
2021-11-19 01:00:53 -0800

    added hash

--- google/auth/transport/cpp/signer.cpp
+++ google/auth/transport/cpp/signer.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <tchar.h>
+#include <bcrypt.h>
 
 #define MY_ENCODING_TYPE  (PKCS_7_ASN_ENCODING | X509_ASN_ENCODING)
 #define SIGNER_NAME L"localhost"
@@ -18,18 +19,33 @@ class WindowsSigner {
         void GetSignerCert();
         void Sign();
         void GetPrivateKey();
+        void CreateHash();
     private:
         void Cleanup();
         void HandleError(LPTSTR psz);
         CRYPT_SIGN_MESSAGE_PARA CreateSignPara();
 
+        // cert store / cert
         HCERTSTORE hCertStore = NULL;   
-        PCCERT_CONTEXT pSignerCert = NULL; 
+        PCCERT_CONTEXT pSignerCert = NULL;
+        // private key
+        HCRYPTPROV hCryptProv = NULL;
+        DWORD dwKeySpec;
+        // hash
+        BCRYPT_ALG_HANDLE hAlg = NULL;
+        BCRYPT_HASH_HANDLE hHash = NULL;
+        PBYTE pbHashObject = NULL;
+        PBYTE pbHash = NULL;
+        DWORD cbHash = 0;
 };
 
 void WindowsSigner::Cleanup() {
     if(pSignerCert) CertFreeCertificateContext(pSignerCert);
     if(hCertStore) CertCloseStore(hCertStore, CERT_CLOSE_STORE_CHECK_FLAG);
+    if(hAlg) BCryptCloseAlgorithmProvider(hAlg,0);
+    if (hHash) BCryptDestroyHash(hHash);
+    if(pbHashObject) HeapFree(GetProcessHeap(), 0, pbHashObject);
+    if(pbHash) HeapFree(GetProcessHeap(), 0, pbHash);
 }
 
 WindowsSigner::~WindowsSigner() {
@@ -150,8 +166,6 @@ void WindowsSigner::Sign() {
 }
 
 void WindowsSigner::GetPrivateKey() {
-    DWORD dwKeySpec;
-    HCRYPTPROV hCryptProv;
     if(!(CryptAcquireCertificatePrivateKey(
         pSignerCert,
         CRYPT_ACQUIRE_ALLOW_NCRYPT_KEY_FLAG,
@@ -166,12 +180,87 @@ void WindowsSigner::GetPrivateKey() {
     }
 }
 
+void WindowsSigner::CreateHash() {
+    #define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+    #define STATUS_UNSUCCESSFUL ((NTSTATUS)0xC0000001L)
+
+    static const BYTE rgbMsg[] = {0x61, 0x62, 0x63};
+    NTSTATUS                status          = STATUS_UNSUCCESSFUL;
+    DWORD                   cbData          = 0,
+                            cbHashObject    = 0;
+
+    //open an algorithm handle
+    if(!NT_SUCCESS(status = BCryptOpenAlgorithmProvider(&hAlg, BCRYPT_SHA256_ALGORITHM, NULL, 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptOpenAlgorithmProvider\n", status);
+        goto Cleanup;
+    }
+
+    //calculate the size of the buffer to hold the hash object
+    if(!NT_SUCCESS(status = BCryptGetProperty(hAlg, BCRYPT_OBJECT_LENGTH, (PBYTE)&cbHashObject, sizeof(DWORD), &cbData, 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptGetProperty\n", status);
+        goto Cleanup;
+    }
+
+    //allocate the hash object on the heap
+    pbHashObject = (PBYTE)HeapAlloc (GetProcessHeap(), 0, cbHashObject);
+    if(NULL == pbHashObject)
+    {
+        wprintf(L"**** memory allocation failed\n");
+        goto Cleanup;
+    }
+
+   //calculate the length of the hash
+    if(!NT_SUCCESS(status = BCryptGetProperty(hAlg, BCRYPT_HASH_LENGTH, (PBYTE)&cbHash, sizeof(DWORD), &cbData, 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptGetProperty\n", status);
+        goto Cleanup;
+    }
+
+    //allocate the hash buffer on the heap
+    pbHash = (PBYTE)HeapAlloc (GetProcessHeap(), 0, cbHash);
+    if(NULL == pbHash)
+    {
+        wprintf(L"**** memory allocation failed\n");
+        goto Cleanup;
+    }
+
+    //create a hash
+    if(!NT_SUCCESS(status = BCryptCreateHash(hAlg, &hHash, pbHashObject, cbHashObject, NULL, 0, 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptCreateHash\n", status);
+        goto Cleanup;
+    }
+    
+
+    //hash some data
+    if(!NT_SUCCESS(status = BCryptHashData(hHash, (PBYTE)rgbMsg, sizeof(rgbMsg), 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptHashData\n", status);
+        goto Cleanup;
+    }
+    
+    //close the hash
+    if(!NT_SUCCESS(status = BCryptFinishHash(hHash, pbHash, cbHash, 0)))
+    {
+        wprintf(L"**** Error 0x%x returned by BCryptFinishHash\n", status);
+        goto Cleanup;
+    }
+
+    printf("Hash size is: %lu\n", cbHash);
+    printf("Success!\n");
+
+Cleanup:
+    return;
+}
+
 static PyObject* sign(PyObject *self, PyObject *args) {
     printf("calling sign\n");
     WindowsSigner signer;
     signer.GetSignerCert();
     signer.GetPrivateKey();
-    //signer.Sign();
+    signer.CreateHash();
     Py_INCREF(Py_None);
     return Py_None; 
 }
