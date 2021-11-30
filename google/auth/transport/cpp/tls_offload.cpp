6bd5b3e4b5f55be0fb3f0ea5a97986679915ed1e
user <user@DESKTOP-F38E1TO>
2021-11-30 15:45:27 -0800

    simplify tls_offload.cpp

--- google/auth/transport/cpp/tls_offload.cpp
+++ google/auth/transport/cpp/tls_offload.cpp
@@ -19,34 +19,22 @@
 
 #ifdef _WIN32
 #include <Python.h>
-// #include "signer.h"
 #endif
 
 namespace {
-
-static bool EnableLogging = false;
-
-void LogInfo(const std::string& message) {
-  if (EnableLogging) {
-    std::cout << "tls_offload.cpp: " << message << "...." << std::endl;
-  }
-}
 
 template <typename T, typename Ret, Ret (*Deleter)(T *)>
 struct OpenSSLDeleter {
   void operator()(T *t) const { Deleter(t); }
 };
- 
 struct OpenSSLFreeDeleter {
   void operator()(unsigned char *buf) const { OPENSSL_free(buf); }
 };
- 
 template <typename T, void (*Deleter)(T *)>
 using OwnedOpenSSLPtr = std::unique_ptr<T, OpenSSLDeleter<T, void, Deleter>>;
 template <typename T, int (*Deleter)(T *)>
 using OwnedOpenSSLPtrIntRet =
     std::unique_ptr<T, OpenSSLDeleter<T, int, Deleter>>;
- 
 using OwnedBIO = OwnedOpenSSLPtrIntRet<BIO, BIO_free>;
 using OwnedENGINE = OwnedOpenSSLPtrIntRet<ENGINE, ENGINE_free>;
 using OwnedEVP_MD_CTX = OwnedOpenSSLPtr<EVP_MD_CTX, EVP_MD_CTX_free>;
@@ -59,17 +47,38 @@ using OwnedX509_PUBKEY = OwnedOpenSSLPtr<X509_PUBKEY, X509_PUBKEY_free>;
 using OwnedX509 = OwnedOpenSSLPtr<X509, X509_free>;
 using OwnedOpenSSLBuffer = std::unique_ptr<uint8_t, OpenSSLFreeDeleter>;
 
-void FreeExData(void *parent, void *ptr, CRYPTO_EX_DATA *ad, int idx, long argl,
-                void *argp) {
-  LogInfo("calling FreeExData");
+static bool EnableLogging = false;
+static int rsa_ex_index = -1, ec_ex_index = -1;
+static EVP_PKEY_METHOD *custom_rsa_pkey_method, *custom_ec_pkey_method;
+static ENGINE *custom_engine = nullptr;
+
+static int EngineGetMethods(ENGINE *e, EVP_PKEY_METHOD **out_method, const int **out_nids, int nid) {
+  if (!out_method) {
+    static const int kNIDs[] = {EVP_PKEY_EC, EVP_PKEY_RSA};
+    *out_nids = kNIDs;
+    return sizeof(kNIDs) / sizeof(kNIDs[0]);
+  }
+ 
+  switch (nid) {
+    case EVP_PKEY_EC:
+      *out_method = custom_ec_pkey_method;
+      return 1;
+    case EVP_PKEY_RSA:
+      *out_method = custom_rsa_pkey_method;
+      return 1;
+  }
+  return 0;
 }
 
-static int rsa_ex_index = -1, ec_ex_index = -1;
+void LogInfo(const std::string& message) {
+  if (EnableLogging) {
+    std::cout << "tls_offload.cpp: " << message << "...." << std::endl;
+  }
+}
 
 bool InitExData() {
-  rsa_ex_index = RSA_get_ex_new_index(0, nullptr, nullptr, nullptr, FreeExData);
-  ec_ex_index =
-      EC_KEY_get_ex_new_index(0, nullptr, nullptr, nullptr, FreeExData);
+  rsa_ex_index = RSA_get_ex_new_index(0, nullptr, nullptr, nullptr, nullptr);
+  ec_ex_index = EC_KEY_get_ex_new_index(0, nullptr, nullptr, nullptr, nullptr);
   if (rsa_ex_index < 0 || ec_ex_index < 0) {
     fprintf(stderr, "Error allocating ex data.\n");
     return false;
@@ -77,62 +86,28 @@ bool InitExData() {
   return true;
 }
  
-bool SetCustomKey(RSA *rsa, CustomKey *key) {
-  LogInfo("setting RSA custom key");
-  if (!RSA_set_ex_data(rsa, rsa_ex_index, key)) {
-    return false;
-  }
-  return true;
-}
- 
-bool SetCustomKey(EC_KEY *ec_key, CustomKey *key) {
-  LogInfo("setting EC custom key");
-  if (!EC_KEY_set_ex_data(ec_key, ec_ex_index, key)) {
-    return false;
-  }
-  return true;
-}
- 
 bool SetCustomKey(EVP_PKEY *pkey, CustomKey *key) {
   if (EVP_PKEY_id(pkey) == EVP_PKEY_RSA) {
+    LogInfo("setting RSA custom key");
     RSA *rsa = EVP_PKEY_get0_RSA(pkey);
-    return rsa && SetCustomKey(rsa, key);
+    return rsa && RSA_set_ex_data(rsa, rsa_ex_index, key);
   }
   if (EVP_PKEY_id(pkey) == EVP_PKEY_EC) {
+    LogInfo("setting EC custom key");
     EC_KEY *ec_key = EVP_PKEY_get0_EC_KEY(pkey);
-    return ec_key && SetCustomKey(ec_key, key);
+    return ec_key && EC_KEY_set_ex_data(ec_key, ec_ex_index, key);
   }
   return false;
 }
- 
-CustomKey *GetCustomKey(const RSA *rsa) {
-  LogInfo("getting RSA custom key");
-  return static_cast<CustomKey*>(RSA_get_ex_data(rsa, rsa_ex_index));
-}
- 
-CustomKey *GetCustomKey(const EC_KEY *ec_key) {
-  LogInfo("getting EC custom key");
-  return static_cast<CustomKey*>(EC_KEY_get_ex_data(ec_key, ec_ex_index));
-}
- 
+
 CustomKey *GetCustomKey(EVP_PKEY *pkey) {
   if (EVP_PKEY_id(pkey) == EVP_PKEY_RSA) {
     const RSA *rsa = EVP_PKEY_get0_RSA(pkey);
-    if (rsa) {
-      LogInfo("rsa exists in GetCustomKey");
-    } else {
-      LogInfo("rsa doesn't exist in GetCustomKey");
-    }
-    return rsa ? GetCustomKey(rsa) : nullptr;
+    return rsa ? static_cast<CustomKey*>(RSA_get_ex_data(rsa, rsa_ex_index)) : nullptr;
   }
   if (EVP_PKEY_id(pkey) == EVP_PKEY_EC) {
     const EC_KEY *ec_key = EVP_PKEY_get0_EC_KEY(pkey);
-    if (ec_key) {
-      LogInfo("ec_key exists in GetCustomKey");
-    } else {
-      LogInfo("ec_key doesn't exist in GetCustomKey");
-    }
-    return ec_key ? GetCustomKey(ec_key) : nullptr;
+    return ec_key ? static_cast<CustomKey*>(EC_KEY_get_ex_data(ec_key, ec_ex_index)) : nullptr;
   }
   return nullptr;
 }
@@ -152,13 +127,11 @@ int CustomDigestSign(EVP_MD_CTX *ctx, unsigned char *sig, size_t *sig_len,
     return 0;
   }
   if (EnableLogging) {
-    std::cout << "tls_offload.cpp: " << "before calling key->Sign, " << "sig len: " << *sig_len << std::endl;
-    printf("key address: %p\n", key);
+    std::cout << "tls_offload.cpp: " << "before calling key->Sign, sig len: " << *sig_len << std::endl;
   }
   int res = key->Sign(sig, sig_len, tbs, tbs_len);
   if (EnableLogging) {
-    std::cout << "tls_offload.cpp: " << "after calling key->Sign, " << "sig len: " << *sig_len 
-      << "\nsignature: " << *sig << "\nkey->sign_func result: " << res << std::endl;
+    std::cout << "tls_offload.cpp: after calling key->Sign, sig len: " << *sig_len << ", result: " << res << std::endl;
   }
   return res;  
 }
@@ -188,44 +161,6 @@ OwnedEVP_PKEY_METHOD MakeCustomMethod(int nid) {
   EVP_PKEY_meth_set_digestsign(method.get(), CustomDigestSign);
   return method;
 }
- 
-static EVP_PKEY_METHOD *custom_rsa_pkey_method, *custom_ec_pkey_method;
-static ENGINE *custom_engine = nullptr;
-
-static int EngineGetMethods(ENGINE *e, EVP_PKEY_METHOD **out_method,
-                            const int **out_nids, int nid) {
-  if (!out_method) {
-    static const int kNIDs[] = {EVP_PKEY_EC, EVP_PKEY_RSA};
-    *out_nids = kNIDs;
-    return sizeof(kNIDs) / sizeof(kNIDs[0]);
-  }
- 
-  switch (nid) {
-    case EVP_PKEY_EC:
-      *out_method = custom_ec_pkey_method;
-      return 1;
-    case EVP_PKEY_RSA:
-      *out_method = custom_rsa_pkey_method;
-      return 1;
-  }
-  return 0;
-}
- 
-static bool InitEngine() {
-  custom_rsa_pkey_method = MakeCustomMethod(EVP_PKEY_RSA).release();
-  custom_ec_pkey_method = MakeCustomMethod(EVP_PKEY_EC).release();
-  if (!custom_rsa_pkey_method || !custom_ec_pkey_method) {
-    return false;
-  }
- 
-  OwnedENGINE engine(ENGINE_new());
-  if (!engine ||
-      !ENGINE_set_pkey_meths(engine.get(), EngineGetMethods)) {
-    return false;
-  }
-  custom_engine = engine.release();
-  return true;
-}
 
 OwnedEVP_PKEY ConfigureCertAndCustomKey(CustomKey *custom_key, X509 *cert) {
   unsigned char *spki = nullptr;
@@ -250,20 +185,32 @@ OwnedEVP_PKEY ConfigureCertAndCustomKey(CustomKey *custom_key, X509 *cert) {
   return wrapped;
 }
 
-static OwnedX509 CertFromPEM(const char *pem) {
-  OwnedBIO bio(BIO_new_mem_buf(pem, strlen(pem)));
-  if (!bio) {
-    return nullptr;
+static bool InitEngine() {
+  custom_rsa_pkey_method = MakeCustomMethod(EVP_PKEY_RSA).release();
+  custom_ec_pkey_method = MakeCustomMethod(EVP_PKEY_EC).release();
+  if (!custom_rsa_pkey_method || !custom_ec_pkey_method) {
+    return false;
   }
-  return OwnedX509(
-      PEM_read_bio_X509(bio.get(), nullptr, nullptr, nullptr));
+ 
+  OwnedENGINE engine(ENGINE_new());
+  if (!engine || !ENGINE_set_pkey_meths(engine.get(), EngineGetMethods)) {
+    return false;
+  }
+  custom_engine = engine.release();
+  return true;
 }
 
 static bool ServeTLS(CustomKey *custom_key, const char *cert, SSL_CTX *ctx) {
   LogInfo("calling ServeTLS");
 
-  LogInfo("create x509 using CertFromPEM");
-  OwnedX509 x509 = CertFromPEM(cert);
+  LogInfo("create x509 using the provided cert");
+  OwnedBIO bio(BIO_new_mem_buf(cert, strlen(cert)));
+  if (!bio) {
+    LogInfo("failed to read cert into bio");
+    return false;
+  }
+  OwnedX509 x509 = OwnedX509(PEM_read_bio_X509(bio.get(), nullptr, nullptr, nullptr));
+
   LogInfo("create custom key");
   OwnedEVP_PKEY wrapped_key = ConfigureCertAndCustomKey(custom_key, x509.get());
   if (!wrapped_key) {
