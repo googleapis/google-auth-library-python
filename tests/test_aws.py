a764bd4ed8353c9f5abb1b37d9d34f85e0d72a08
Sai Sunder <saisunder92@gmail.com>
2022-02-03 20:17:41 +0000

    add coverage

--- tests/test_aws.py
+++ tests/test_aws.py
@@ -1114,6 +1114,34 @@ class TestCredentials(object):
         )
 
     @mock.patch("google.auth._helpers.utcnow")
+    def test_retrieve_subject_token_session_error_idmsv2(
+        self, utcnow
+    ):
+        utcnow.return_value = datetime.datetime.strptime(
+            self.AWS_SIGNATURE_TIME, "%Y-%m-%dT%H:%M:%SZ"
+        )
+        request = self.make_mock_request(
+            session_token_status=http_client.UNAUTHORIZED,
+            session_token_data="unauthorized",
+        )
+        credential_source_token_url = self.CREDENTIAL_SOURCE.copy()
+        credential_source_token_url["aws_session_token_url"] = AWS_SESSION_TOKEN_URL
+        credentials = self.make_credentials(credential_source=credential_source_token_url)
+
+        with pytest.raises(exceptions.RefreshError) as excinfo:
+            credentials.retrieve_subject_token(request)
+
+        assert excinfo.match(r"Unable to retrieve AWS Session Token")
+
+        # Assert session token request
+        self.assert_aws_metadata_request_kwargs(
+            request.call_args_list[0][1],
+            AWS_SESSION_TOKEN_URL,
+            {"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+            "PUT"
+        )
+
+    @mock.patch("google.auth._helpers.utcnow")
     def test_retrieve_subject_token_success_permanent_creds_no_environment_vars(
         self, utcnow
     ):
