c9f5eaacfe41c4b677d7acd06c2f295871e92612
Carl Lundin <clundin@google.com>
2022-09-01 09:48:07 -0700

    Fix cover lint.

--- tests/test__oauth2client.py
+++ tests/test__oauth2client.py
@@ -25,6 +25,7 @@ try:
     import oauth2client.contrib.gce  # type: ignore
     import oauth2client.service_account  # type: ignore
 except ImportError:  # pragma: no cover
+    # pragma: no cover
     pytest.skip(
         "Skipping oauth2client tests since oauth2client is not installed.",
         allow_module_level=True,
