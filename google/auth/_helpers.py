bda4f39ccb8efeffc0eaecfa227bad08a8e9162f
DanieleQuasimodo <134523456+DanieleQuasimodo@users.noreply.github.com>
2023-09-23 12:56:17 +0200

    test: Added some new tests for the parse_content_type function in _helpers and its use in _metadata

--- google/auth/_helpers.py
+++ google/auth/_helpers.py
@@ -30,26 +30,6 @@ from google.auth import exceptions
 REFRESH_THRESHOLD = datetime.timedelta(seconds=20)
 
 
-def parse_content_type(header_value):
-    """Parse a 'content-type' header value to get just the plain media-type (without parameters).
-
-    This is done using the class Message from email.message as suggested in PEP 594
-        (because the cgi is now deprecated and will be removed in python 3.13,
-        see https://peps.python.org/pep-0594/#cgi).
-
-    Args:
-        header_value (str): The value of a 'content-type' header as string.
-
-    Returns:
-        str: A string with just the lowercase media-type from the parsed 'content-type' header.
-            If the provided content-type is not parsable, returns 'text/plain',
-            the default value for textual files.
-    """
-    m = Message()
-    m['content-type'] = header_value
-    return m.get_content_type()
-
-
 def copy_docstring(source_class):
     """Decorator that copies a method's docstring from another class.
 
@@ -82,6 +62,26 @@ def copy_docstring(source_class):
         return method
 
     return decorator
+
+
+def parse_content_type(header_value):
+    """Parse a 'content-type' header value to get just the plain media-type (without parameters).
+
+    This is done using the class Message from email.message as suggested in PEP 594
+        (because the cgi is now deprecated and will be removed in python 3.13,
+        see https://peps.python.org/pep-0594/#cgi).
+
+    Args:
+        header_value (str): The value of a 'content-type' header as a string.
+
+    Returns:
+        str: A string with just the lowercase media-type from the parsed 'content-type' header.
+            If the provided content-type is not parsable, returns 'text/plain',
+            the default value for textual files.
+    """
+    m = Message()
+    m["content-type"] = header_value
+    return m.get_content_type() # Despite the name, actually returns just the media-type
 
 
 def utcnow():
