31ab41aa7f36150b7eec81f0b300d0cbd9fb0156
ohmayr <omairnaveed@ymail.com>
2024-08-02 22:03:28 +0000

    feat: implement async request and response classes

--- google/auth/aio/transport/requests.py
+++ google/auth/aio/transport/requests.py
@@ -14,3 +14,56 @@
 
 """Transport adapter for Asynchronous HTTP Requests.
 """
+
+import aiohttp
+
+
+class Response:
+
+    """
+    Instances of Response class are returned by 
+    ``google.auth.aio.transport.requests.AuthorizedSession`` and provide methods to interact
+    with the response data.
+    
+    
+    Args:
+        response (aiohttp.ClientResponse): An instance of aiohttp.ClientResponse.
+
+    Attributes:
+        status (int): The HTTP status code of the response.
+        headers (dict): A dictionary of HTTP headers.
+        content (coroutine): The raw content of the response.
+
+    
+    """
+    
+    def __init__(self, response: aiohttp.ClientResponse):
+        self._response = response
+
+    @property
+    def status(self):
+        return self._response.status
+    
+    @property
+    def headers(self):
+        return self._response.headers
+    
+    @property
+    def content(self):
+        return self._response.content
+    
+    async def text(self):
+        return await self._response.text()
+    
+    async def json(self):
+        return await self._response.json()
+    
+    async def read(self):
+        return await self._response.read()
+    
+    async def close(self):
+        return await self._response.close()
+    
+
+class Request:
+    pass
--- tests/transport/aio/test_requests.py
+++ tests/transport/aio/test_requests.py
@@ -14,3 +14,50 @@
 
 import google.auth.aio.transport.requests as requests_aio
 import pytest # type: ignore
+import aiohttp
+from unittest.mock import Mock, AsyncMock
+
+
+@pytest.fixture
+def mock_response():
+    response = Mock(spec=aiohttp.ClientResponse)
+    response.status = 200
+    response.headers = {'Content-Type': 'application/json', 'Content-Length': "100"}
+    response.text = AsyncMock(return_value="Stout Infant is the world's tiniest fish.")
+    response.json = AsyncMock(return_value={"Goldie":"The oldest known Goldfish."})
+    response.read = AsyncMock(return_value=b'Catfish have over 27,000 tastebuds.')
+
+    response.content = Mock()
+    response.content.read = AsyncMock(return_value=b"Cavefish have no sight.")
+
+
+    return requests_aio.Response(response)
+
+
+class TestResponse(object):
+     
+    @pytest.mark.asyncio
+    async def test_response_status(self, mock_response):
+        assert mock_response.status == 200
+    
+    @pytest.mark.asyncio
+    async def test_response_headers(self, mock_response):
+        assert mock_response.headers['Content-Type'] == 'application/json'
+        assert mock_response.headers['Content-Length'] == "100"
+    
+    @pytest.mark.asyncio
+    async def test_response_content(self, mock_response):
+        assert await mock_response.content.read() == b"Cavefish have no sight."
+
+    @pytest.mark.asyncio
+    async def test_response_text(self, mock_response):
+        assert await mock_response.text() == "Stout Infant is the world's tiniest fish."
+    
+    @pytest.mark.asyncio
+    async def test_response_json(self, mock_response):
+        assert await mock_response.json() == {"Goldie": "The oldest known Goldfish."}
+    
+    @pytest.mark.asyncio
+    async def test_response_read(self, mock_response):
+        assert await mock_response.read() == b'Catfish have over 27,000 tastebuds.'
+    
