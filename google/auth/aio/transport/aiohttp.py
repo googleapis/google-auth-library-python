60d06d47150f93437dd7ab3e705915fe0396df50
ohmayr <omairnaveed@ymail.com>
2024-08-07 21:46:13 +0000

    update docstring to be specific to aiohttp

--- google/auth/aio/transport/aiohttp.py
+++ google/auth/aio/transport/aiohttp.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Transport adapter for Asynchronous HTTP Requests.
+"""Transport adapter for AIOHTTP Requests.
 """
 
 try:
