96756c0796b87f215ab87e5679af3aa44a56e747
Sai Sunder Srinivasan <saisunder@google.com>
2024-10-14 23:36:18 +0000

    Add test for ssj

--- tests/showcase/test_service_account.py
+++ tests/showcase/test_service_account.py
@@ -34,31 +34,11 @@ def echo():
     # Optional: Clean up resources if needed after the test
     # e.g., echo_client.close()
 
-def test_unary_with_request_object(echo):
-    response = echo.echo(
-        showcase.EchoRequest(
-            content="The hail in Wales falls mainly on the snails.",
-            request_id="some_value",
-            other_request_id="",
+def test_ssj_with_scopes(echo):
+    response = echo.echo_authentication(
+        showcase.EchoAuthenticationRequest(
         )
     )
-    assert response.content == "The hail in Wales falls mainly on the snails."
-    assert response.request_id == "some_value"
-    assert response.other_request_id == ""
 
-    # Repeat the same test but this time without `request_id`` set
-    # The `request_id` field should be automatically populated with
-    # a UUID4 value if it is not set.
-    # See https://google.aip.dev/client-libraries/4235
-    response = echo.echo(
-        showcase.EchoRequest(
-            content="The hail in Wales falls mainly on the snails.",
-        )
-    )
-    assert response.content == "The hail in Wales falls mainly on the snails."
-    # Ensure that the uuid4 field is set according to AIP 4235
-    assert re.match(UUID4_RE, response.request_id)
-    assert len(response.request_id) == 36
-    # Ensure that the uuid4 field is set according to AIP 4235
-    assert re.match(UUID4_RE, response.other_request_id)
-    assert len(response.other_request_id) == 36
+    # Handle the response
+    print(response)
