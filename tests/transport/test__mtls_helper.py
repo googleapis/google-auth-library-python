6133e8f188c8dfae079925a2c7fdb591349c9ff5
Harkamal Jot Singh Kumar <harkamalj@google.com>
2025-03-04 00:49:29 +0000

    remove test using deprecated code

--- tests/transport/test__mtls_helper.py
+++ tests/transport/test__mtls_helper.py
@@ -620,19 +620,6 @@ class TestGetClientCertAndKey(object):
 
 
 class TestDecryptPrivateKey(object):
-    def test_success(self):
-        decrypted_key = _mtls_helper.decrypt_private_key(
-            ENCRYPTED_EC_PRIVATE_KEY, PASSPHRASE_VALUE
-        )
-        private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, decrypted_key)
-        public_key = crypto.load_publickey(crypto.FILETYPE_PEM, EC_PUBLIC_KEY)
-        x509 = crypto.X509()
-        x509.set_pubkey(public_key)
-
-        # Test the decrypted key works by signing and verification.
-        signature = crypto.sign(private_key, b"data", "sha256")
-        crypto.verify(x509, signature, b"data", "sha256")
-
     def test_crypto_error(self):
         with pytest.raises(crypto.Error):
             _mtls_helper.decrypt_private_key(
