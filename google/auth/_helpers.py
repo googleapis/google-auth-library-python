05d176f204a3fd34e56b2b158a1cba44911bf4d5
ohmayr <omairn@google.com>
2025-03-24 16:49:34 +0000

    fix mypy type issue

--- google/auth/_helpers.py
+++ google/auth/_helpers.py
@@ -22,7 +22,7 @@ import hashlib
 import json
 import logging
 import sys
-from typing import Any, Mapping, Optional, Union
+from typing import Any, Dict, Mapping, Optional, Union
 import urllib
 
 from google.auth import exceptions
@@ -312,7 +312,7 @@ def _hash_sensitive_info(data: Union[dict, list]) -> Union[dict, list, str]:
 
     """
     if isinstance(data, dict):
-        hashed_data = {}
+        hashed_data : Dict[Any, Union[Optional[str], dict, list]] = {}
         for key, value in data.items():
             if key in _SENSITIVE_FIELDS and not isinstance(value, (dict, list)):
                 hashed_data[key] = _hash_value(value, key)
