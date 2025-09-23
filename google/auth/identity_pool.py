32cc7a0370c41432c8004e5131abc8dd95f9a218
Sai Sunder Srinivasan <saisunder@google.com>
2025-09-23 20:51:49 +0000

    fix: Deprecating generic load methods and adding warnings on few cred types

--- google/auth/identity_pool.py
+++ google/auth/identity_pool.py
@@ -497,6 +497,14 @@ class Credentials(external_account.Credentials):
     def from_info(cls, info, **kwargs):
         """Creates an Identity Pool Credentials instance from parsed external account info.
 
+        **IMPORTANT**:
+        This method does not validate the credential configuration. A security
+        risk occurs when a credential configuration configured with malicious urls
+        is used.
+        When the credential configuration is accepted from an
+        untrusted source, you should validate it before using with this method.
+        Refer https://cloud.google.com/docs/authentication/external/externally-sourced-credentials for more details.
+
         Args:
             info (Mapping[str, str]): The Identity Pool external account info in Google
                 format.
@@ -516,6 +524,14 @@ class Credentials(external_account.Credentials):
     @classmethod
     def from_file(cls, filename, **kwargs):
         """Creates an IdentityPool Credentials instance from an external account json file.
+
+        **IMPORTANT**:
+        This method does not validate the credential configuration. A security
+        risk occurs when a credential configuration configured with malicious urls
+        is used.
+        When the credential configuration is accepted from an
+        untrusted source, you should validate it before using with this method.
+        Refer https://cloud.google.com/docs/authentication/external/externally-sourced-credentials for more details.
 
         Args:
             filename (str): The path to the IdentityPool external account json file.
