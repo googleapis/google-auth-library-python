8a9cb7bfbf8de7d3f26758f82bdaf89cd23a303c
agrawalradhika-cell <agrawalradhika@google.com>
2025-12-15 23:25:01 +0530

    feat: Add retry logic when certificate mismatch for existing credentials & Agent Identity workloads (#1841)
    feat: Add retry logic when certificate mismatch for existing credentials
    & Agent Identity workloads
    
    This change introduces retry support when requests are created for
    existing credentials and Agent Identities on GKE and Cloud Run
    Workloads. When 401(Unauthorized) error is created, due to certificate
    at time of configuration of mTLS channel being different from the
    current certificate, a retry is added to the request by configuring the
    mTLS channel with the current certificate.
    
    ---------
    
    Signed-off-by: Radhika Agrawal <agrawalradhika@google.com>
    Co-authored-by: nbayati <99771966+nbayati@users.noreply.github.com>
    Co-authored-by: Andy Zhao <andyzhao@google.com>

--- google/auth/transport/_mtls_helper.py
+++ google/auth/transport/_mtls_helper.py
@@ -20,6 +20,7 @@ from os import environ, getenv, path
 import re
 import subprocess
 
+from google.auth import _agent_identity_utils
 from google.auth import environment_vars
 from google.auth import exceptions
 
@@ -474,3 +475,29 @@ def check_use_client_cert():
             ) as e:
                 _LOGGER.debug("error decoding certificate: %s", e)
         return False
+
+
+def check_parameters_for_unauthorized_response(cached_cert):
+    """Returns the cached and current cert fingerprint for reconfiguring mTLS.
+
+    Args:
+        cached_cert(bytes): The cached client certificate.
+
+    Returns:
+        bytes: The client callback cert bytes.
+        bytes: The client callback key bytes.
+        str: The base64-encoded SHA256 cached fingerprint.
+        str: The base64-encoded SHA256 current cert fingerprint.
+    """
+    call_cert_bytes, call_key_bytes = _agent_identity_utils.call_client_cert_callback()
+    cert_obj = _agent_identity_utils.parse_certificate(call_cert_bytes)
+    current_cert_fingerprint = _agent_identity_utils.calculate_certificate_fingerprint(
+        cert_obj
+    )
+    if cached_cert:
+        cached_fingerprint = _agent_identity_utils.get_cached_cert_fingerprint(
+            cached_cert
+        )
+    else:
+        cached_fingerprint = current_cert_fingerprint
+    return call_cert_bytes, call_key_bytes, cached_fingerprint, current_cert_fingerprint
