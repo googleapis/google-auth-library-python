5a65b78cd753e001c95d46917e1a22618a0169f4
Chalmer Lowe <chalmerlowe@google.com>
2025-12-26 08:04:00 -0500

    Update tests_async/transport/test_aiohttp_requests.py

--- tests_async/transport/test_aiohttp_requests.py
+++ tests_async/transport/test_aiohttp_requests.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from unittest import mock
+
 import aiohttp  # type: ignore
 from aioresponses import aioresponses, core  # type: ignore
 import pytest  # type: ignore
